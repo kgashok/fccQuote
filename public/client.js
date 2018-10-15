2e4f3f49e624ecd8f54117c9c279d173ad1873d7
Glitch (mindless-flute) <none>
2018-10-15 12:44:46 +0000

    Fixed #12 - refactored by adding addQuoteToDisplay function

--- public/client.js
+++ public/client.js
@@ -22,24 +22,54 @@ function isJsonString(str) {
   return true;
 }
 
+function addQuoteToDisplay (response) {
+  //if (response.quoteText !== undefined) {            
+    var tweeter = '<div id=\"share\">\
+      <a target=\"_blank\" id=\"t\" href=\"http://twitter.com/home?status=';
+    tweeter = tweeter + response.quoteText; 
+    tweeter = tweeter + ' @lifebalance" title=\"Write\"></a></div>';
+    var responseHTML = tweeter + '\
+      <blockquote class="quote">\
+        <strong><cite>\
+          <a target=\"_blank\" id = "quote" href=\"'+response.quoteLink+'\">\
+            '+response.quoteText+'</a>\
+        </cite></strong><br/>\
+        <small>\
+          <a target="_blank" href="http://en.wikipedia.org/wiki/'+response.quoteAuthor+'\">\
+            '+response.quoteAuthor+'</a>\
+        </small>\
+      </blockquote>';
+
+    //console.log (responseHTML);
+    $('<ul></ul>').html(responseHTML).appendTo('ul#responses');
+  //}
+}
+
 $(function() {
 
-  // This is invoked to refresh previously asked 
-  // questions, if any? 
+  // This is invoked to refresh previously received
+  // quotations, if any 
   $.get('/responses', function funcInvokedAfterGET(responses) {
     // for e.g. https://roomy-plate.gomix.me/responses
     // refresh and update the DOM with 
     // previous Q and As from the same session
     // along with any valid ones post the POST request
-    if ($('ul#responses').is(':empty') ){
-        
+    console.log("**** Inside get() call ");
+    if ($('ul#responses').children().length == 0){
+    //if ($('ul#responses').is(':empty') ){
+      console.log("Nothing on display!"); 
       responses.forEach(function(response) {
-        $('<li></li>').text(response).appendTo('ul#responses');
+        //$('<li></li>').text(response).appendTo('ul#responses');
+        addQuoteToDisplay(response);
       });
+      
     }
+    /*
+     * this is not required for now...
     else {
        $('<li></li>').text(responses[0]).appendTo('ul#responses');
     }
+    */
   });
 
 
@@ -76,28 +106,7 @@ $(function() {
                                 + "-" 
                                 + response.quoteAuthor).appendTo('ul#responses');
             */
-            
-            //if (response.quoteText !== undefined) {            
-              var tweeter = '<div id=\"share\">\
-                <a target=\"_blank\" id=\"t\" href=\"http://twitter.com/home?status=';
-              tweeter = tweeter + response.quoteText; 
-              tweeter = tweeter + ' @lifebalance" title=\"Write\"></a></div>';
-              var responseHTML = tweeter + '\
-                <blockquote class="quote">\
-                  <strong><cite>\
-                    <a target=\"_blank\" id = "quote" href=\"'+response.quoteLink+'\">\
-                      '+response.quoteText+'</a>\
-                  </cite></strong><br/>\
-                  <small>\
-                    <a target="_blank" href="http://en.wikipedia.org/wiki/'+response.quoteAuthor+'\">\
-                      '+response.quoteAuthor+'</a>\
-                  </small>\
-                </blockquote>';
-
-              //console.log (responseHTML);
-              $('<ul></ul>').html(responseHTML).appendTo('ul#responses');
-            //}
-            
+            addQuoteToDisplay(response);
           });
           // $("#responses").html(data);  
         });
